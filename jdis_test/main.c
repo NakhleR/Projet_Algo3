3bc119504e225cf8eb069cc29db9f9be13a42cae
NakhleR <Nakhler2k2@gmail.com>
2025-05-11 18:41:23 +0200

    Manda Changes

--- jdis_test/main.c
+++ jdis_test/main.c
@@ -1,27 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include "jdis.h"
 
-void print_help(void);
-void print_usage(void);
-
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("jdis: Missing operand.\n");
-    printf("Try 'jdis --help' for more information.\n");
-    return 1;
-  }
-  if (argc >= 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1],
-      "-?") == 0)) {
-    print_help();
-    return EXIT_SUCCESS;
-  }
-  if (argc >= 2 && strcmp(argv[1], "--usage") == 0) {
-    print_usage();
-    return EXIT_SUCCESS;
-  }
-  return EXIT_SUCCESS;
-}
+#define WORD_LEN_MAX 31
 
 void print_usage(void) {
   printf("Usage: jdis [OPTION]... FILE1 FILE2 [FILE]...\n");
@@ -106,4 +89,38 @@ void print_help(void) {
   printf(
       "White-space and punctuation characters conform to the standard. At most 64 FILEs\n");
   printf("are supported.\n");
+}
+
+int main(int argc, char *argv[]) {
+  if (argc >= 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1],
+        "-?") == 0)) {
+    print_help();
+    return EXIT_SUCCESS;
+  }
+  if (argc >= 2 && strcmp(argv[1], "--usage") == 0) {
+    print_usage();
+    return EXIT_SUCCESS;
+  }
+  if (argc < 3) {
+    printf("jdis: Missing operand.\n");
+    printf("Try 'jdis --help' for more information.\n");
+    return EXIT_FAILURE;
+  }
+  // Charge les mots uniques des deux fichiers
+  hashtable *ht1 = get_unique_words(argv[1]);
+  hashtable *ht2 = get_unique_words(argv[2]);
+  if (ht1 == nullptr || ht2 == nullptr) {
+    hashtable_dispose(&ht1);
+    hashtable_dispose(&ht2);
+    return 1;
+  }
+  // Calcul de la distance : reste à définir
+  //float distance = jaccard_distance(ht1, ht2);
+  //exemple pour tester :
+  double distance = 0.2;
+  printf("%.4f\t%s\t%s\n", distance, argv[1], argv[2]);
+  // Libère la mémoire
+  hashtable_dispose(&ht1);
+  hashtable_dispose(&ht2);
+  return EXIT_SUCCESS;
 }
